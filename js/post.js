31a874dd3af704eb97dd79b79930f98539adc533
unknown <yrc371106@hotmail.com>
2018-01-15 11:08:33 +0800

    Fix bug about 'ImgBox'

--- js/post.js
+++ js/post.js
@@ -305,7 +305,9 @@ function mdx_show_img(){
     $('div.mdui-drawer').before('<div id="img-box" class="mdui-valign"><button class="mdui-btn mdui-btn-icon mdui-ripple mdui-text-color-white mdui-valign mdui-text-center" id="close-img-box"><i class="mdui-icon material-icons">&#xe5cd;</i></button><div class="mdx-si-tip"><p>长按/右键保存图片</p></div></div><div class="mdui-valign mdx-loading-img"><div class="mdui-center"><div class="mdui-spinner"></div></div></div>');
     mdui.updateSpinners();
     $('#img-box').css({'opacity':'1','pointer-events':'auto'});
-    $("meta[name='theme-color']").attr('content',"#212121");
+    if(!sessionStorage.getItem('ns_night-styles')!='true'){
+        $("meta[name='theme-color']").attr('content',"#212121");
+    }
     $('#mdx-share-img').show();
         if(!sessionStorage.getItem('si_imgdata_'+url_hash)){
         html2canvas(document.getElementById("mdx-share-img"),{allowTaint: true}).then(function(canvas){
@@ -359,7 +361,9 @@ function mdx_show_img(){
 }
 $('body').on('click','#close-img-box',function(){
     $('#img-box').css({'opacity':'0','pointer-events':'none'});
-    $("meta[name='theme-color']").attr('content',now_color);
+    if(sessionStorage.getItem('ns_night-styles')!='true'){
+        $("meta[name='theme-color']").attr('content',now_color);
+    }
     window.setTimeout("afterCloseImgBox()",200);
 })
 
