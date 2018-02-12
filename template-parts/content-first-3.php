f491a8a3bd85287b942ba29486dc3b1460c648f9
Yrccondor <yrc371106@hotmail.com>
2018-02-12 19:53:59 +0800

    Add new function 'post def img'

--- template-parts/content-first-3.php
+++ template-parts/content-first-3.php
@@ -1,4 +1,32 @@
-<?php $Imagesurl = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID())) ? wp_get_attachment_url( get_post_thumbnail_id(get_the_ID())):get_template_directory_uri().'/img/dpic.jpg';?>
+<?php if(wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()))){$Imagesurl=wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()));}else{$Imagesurl="";}if(get_option("mdx_post_def_img")=="false" && $Imagesurl == ""){?>
+    <div class="mdui-card postDiv mdui-center mdui-hoverable">
+    <div class="mdui-card-actions">
+        <a href="<?php the_permalink();?>" class="mdui-text-color-theme-accent ainList"><h1><?php the_title();?></h1></a>
+        <?php if(get_option("mdx_echo_post_sum")=="true"){ ?><p class="ct1-p mdui-text-color-black cont2"><?php if(post_password_required()){_e('这篇文章受密码保护，输入密码才能看哦', 'mdx');}else{echo mdx_get_post_excerpt($post, 250);}?><p><?php }?>
+        <div class="mdui-divider underline"></div>
+        <?php
+        $mdx_more_1 = get_option("mdx_post_list_1");
+        $mdx_more_2 = get_option("mdx_post_list_2");
+        if($mdx_more_1=='view'){
+            $mdx_icon_1 = '&#xe417;';
+        }else if($mdx_more_1=='time'){
+            $mdx_icon_1 = '&#xe192;';
+        }else if($mdx_more_1=='comments'){
+            $mdx_icon_1 = '&#xe0cb;';
+        }
+        if($mdx_more_2=='view'){
+            $mdx_icon_2 = '&#xe417;';
+        }else if($mdx_more_2=='time'){
+            $mdx_icon_2 = '&#xe192;';
+        }else if($mdx_more_2=='comments'){
+            $mdx_icon_2 = '&#xe0cb;';
+        }
+        ?>
+        <span class="info">&nbsp;&nbsp;<i class="mdui-icon material-icons info-icon"><?php echo $mdx_icon_1;?></i> <?php if($mdx_more_1=='view'){get_post_views($post->ID);}else if($mdx_more_1=='comments'){comments_popup_link('0', '0', '%');}else if($mdx_more_1=='time'){the_time('Y-m-d');}?>&nbsp;&nbsp;<i class="mdui-icon material-icons info-icon"><?php echo $mdx_icon_2;?></i> <?php if($mdx_more_2=='view'){get_post_views($post->ID);}else if($mdx_more_2=='comments'){comments_popup_link('0', '0', '%');}else if($mdx_more_2=='time'){the_time('Y-m-d');}?></span>
+        <a class="mdui-btn mdui-ripple mdui-ripple-white coun-read mdui-text-color-theme-accent" href="<?php the_permalink();?>"><?php echo get_option("mdx_readmore");?></a>
+    </div>
+</div>
+<?php }else{if($Imagesurl == ""){$Imagesurl=get_template_directory_uri().'/img/dpic.jpg';}?>
 <div class="mdui-card postDiv mdui-center mdui-hoverable">
     <div class="mdui-card-media">
     <img src="<?php echo "$Imagesurl"?>" alt="<?php echo "$Imagesurl"?>" title="<?php the_title();?>">
@@ -33,3 +61,4 @@
         <a class="mdui-btn mdui-ripple mdui-ripple-white coun-read mdui-text-color-theme-accent" href="<?php the_permalink();?>"><?php echo get_option("mdx_readmore");?></a>
     </div>
 </div>
+<?php }?>
