91987cc480d2d3e07d2ae32b273050f7b49e17a2
Yrccondor <yrc371106@hotmail.com>
2018-07-07 22:33:05 +0800

    Improve Real-Time search

--- js/search.js
+++ js/search.js
@@ -1,9 +1,12 @@
-var nowType = '';
-var oldType = '';
-var timeChecker;
+"use strict";
+let nowType = '';
+let oldType = '';
+let timeChecker;
+let waitForNetwork = false;
 $('body').prepend('<div class="OutOfsearchBox"><div class="searchBoxFill"></div></div>');
-$(".seainput").focus(function(){
-    timeChecker = window.setInterval('checkType()',300);
+let stopId;
+let isStoped = false;
+$('.seainput').focus(function(){
     if(sessionStorage.getItem('rts_wra') == 'false'){
         mdui.snackbar({
             message: snackMuti,
@@ -11,28 +14,36 @@ $(".seainput").focus(function(){
             position: 'top',
         });
     }
+    isStoped = false;
+    window.requestAnimationFrame(checkType);
 });
-$(".seainput").blur(function(){
-    window.clearInterval(timeChecker);
+$('.seainput').blur(function(){
+    isStoped = true;
+    window.cancelAnimationFrame(stopId);
 });
 function checkType(){
     if(sessionStorage.getItem('rts_wra') != 'false'){
         nowType = $(".seainput").val();
         if((nowType != '' && oldType == '') || (oldType != '' && nowType != oldType && nowType != '')){
             if(!sessionStorage.getItem("rtsk_"+nowType)){
-                $.get('/wp-json/wp/v2/posts?search='+nowType+'&pre_page=10&context=embed',function(data,status){
-                    if(status=='success'){
-                        setSessionSto(nowType, JSON.stringify(data));
-                        readRes(data, false);
-                    }else{
-                        mdui.snackbar({
-                            message: snackMuti,
-                            timeout: 3000,
-                            position: 'top',
-                        });
-                        sessionStorage.setItem('rts_wra', 'false');
-                    }
-                });
+                if(!waitForNetwork){
+                    waitForNetwork = true;
+                   $.get('/wp-json/wp/v2/posts?search='+nowType+'&pre_page=10&context=embed',function(data,status){
+                        if(status=='success'){
+                            setSessionSto(nowType, JSON.stringify(data));
+                            readRes(data, false);
+                            waitForNetwork = false;
+                        }else{
+                            mdui.snackbar({
+                                message: snackMuti,
+                                timeout: 3000,
+                                position: 'top',
+                            });
+                            sessionStorage.setItem('rts_wra', 'false');
+                            waitForNetwork = false;
+                        }
+                    }); 
+                }
             }else{
                 readRes(sessionStorage.getItem("rtsk_"+nowType), true)
             }
@@ -46,6 +57,11 @@ function checkType(){
             oldType = nowType;
         }
     }
+    if(isStoped){
+        return;
+    }else{
+        stopId = window.requestAnimationFrame(checkType);
+    }
 }
 function setSessionSto(nowType, data){
     if(!sessionStorage.getItem("rtsk_"+nowType)){
@@ -58,23 +74,23 @@ function setSessionSto(nowType, data){
     }
 }
 function readRes(data, ifSession){
-    var data2 = data;
+    let data2 = data;
     if(ifSession){
         data2 = JSON.parse(data);
     }
     $('.OutOfsearchBox').html('<div class="searchBoxFill"></div>');
     if(data2.length > 0){
-        for(i in data2){
-            var postTitleInApi = data2[i].title.rendered;
-            var postTimeInApi = data2[i].date;
-            var postUrlInApi = data2[i].link;
-            var postDesInApi = data2[i].excerpt.rendered;
-            var nowBoxValue =  $('.OutOfsearchBox').html();
+        for(let i in data2){
+            let postTitleInApi = data2[i].title.rendered;
+            let postTimeInApi = data2[i].date;
+            let postUrlInApi = data2[i].link;
+            let postDesInApi = data2[i].excerpt.rendered;
+            let nowBoxValue =  $('.OutOfsearchBox').html();
             $('.OutOfsearchBox').html(nowBoxValue+'<article class="searchCard mdui-shadow-2 mdui-typo"><a href="'+postUrlInApi+'"><h1>'+postTitleInApi+'</h1></a><p>'+postDesInApi.replace(' [&hellip;]', '&hellip;').replace('<p>', '').replace('</p>', '')+'</p><div class="mdui-divider underline"></div><span class="info">&nbsp;&nbsp;<i class="mdui-icon material-icons info-icon">&#xe192;</i> '+postTimeInApi.substring(0,10)+'</span><a class="mdui-btn mdui-ripple mdui-ripple-white coun-read" href="'+postUrlInApi+'">'+moreMuti+'</a></article>');
         }
     }
     if(data2.length == 10){
-        var nowBoxValue10 =  $('.OutOfsearchBox').html();
+        let nowBoxValue10 =  $('.OutOfsearchBox').html();
         $('.OutOfsearchBox').html(nowBoxValue10+'<div class="underRes">'+tipMuti+'</div>');
     }
 }
