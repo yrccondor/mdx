f834375f5cd391c58190ff621eeb102d26521cef
Yrccondor <yrc371106@hotmail.com>
2018-07-17 21:15:42 +0800

    Add function 'author info card'

--- single.php
+++ single.php
@@ -89,7 +89,7 @@ $mdx_index_img=get_option('mdx_index_img');$mdx_side_img=get_option('mdx_side_im
                 <?php if($mdx_share_area=="all" || $mdx_share_area=="china"){include('includes/share_cn.php');}if($mdx_share_area=="all" || $mdx_share_area=="oversea"){include('includes/share_oversea.php');}?>
             </ul>
                 <i class="mdui-icon material-icons">&#xe54e;</i> <?php if (get_the_tags()){the_tags('',' ','');}else{_e('没有标签','mdx');}?><span class="mdui-text-color-black-disabled timeInPost" itemprop="datePublished"><?php if(get_option('mdx_post_edit_time')==="post"){?><i class="mdui-icon material-icons info-icon">&#xe192;</i> <?php the_time('Y-m-d');}else{?><i class="mdui-icon material-icons info-icon">&#xe3c9;</i> <?php the_modified_time('Y-m-d');}?></span>
-                <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?><?php if(get_option('mdx_you_may_like')=="true"){include_once("same_posts.php");}comments_template();?>
+                <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?><?php if(get_option('mdx_author_card')=="true"){include_once("includes/author_card.php");}if(get_option('mdx_you_may_like')=="true"){include_once("same_posts.php");}comments_template();?>
             </div>
 <?php get_template_part('toggleposts')?>
         <div id="indic"></div>
@@ -132,7 +132,7 @@ $mdx_index_img=get_option('mdx_index_img');$mdx_side_img=get_option('mdx_side_im
                 <i class="mdui-icon material-icons">&#xe54e;</i> <?php if (get_the_tags()){the_tags('',' ','');}else{_e('没有标签','mdx');}?><span class="mdui-text-color-black-disabled timeInPost" itemprop="datePublished"><?php if(get_option('mdx_post_edit_time')==="post"){?><i class="mdui-icon material-icons info-icon">&#xe192;</i> <?php the_time('Y-m-d');}else{?><i class="mdui-icon material-icons info-icon">&#xe3c9;</i> <?php the_modified_time('Y-m-d');}?></span>
                 <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?>
             </div>
-            <?php if(get_option('mdx_you_may_like')=="true"){include_once("same_posts.php");}comments_template();?>
+            <?php if(get_option('mdx_author_card')=="true"){include_once("includes/author_card.php");}if(get_option('mdx_you_may_like')=="true"){include_once("same_posts.php");}comments_template();?>
 <?php get_template_part('toggleposts')?>
         <div id="indic"></div>
         
@@ -172,7 +172,7 @@ $mdx_index_img=get_option('mdx_index_img');$mdx_side_img=get_option('mdx_side_im
             </ul>
                 <i class="mdui-icon material-icons">&#xe54e;</i> <?php if (get_the_tags()){the_tags('',' ','');}else{_e('没有标签','mdx');}?><span class="mdui-text-color-black-disabled timeInPost" itemprop="datePublished"><?php if(get_option('mdx_post_edit_time')==="post"){?><i class="mdui-icon material-icons info-icon">&#xe192;</i> <?php the_time('Y-m-d');}else{?><i class="mdui-icon material-icons info-icon">&#xe3c9;</i> <?php the_modified_time('Y-m-d');}?></span>
                 <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?>
-                <?php if(get_option('mdx_you_may_like')=="true"){include_once("same_posts.php");}comments_template();?>
+                <?php if(get_option('mdx_author_card')=="true"){include_once("includes/author_card.php");}if(get_option('mdx_you_may_like')=="true"){include_once("same_posts.php");}comments_template();?>
 </div>
 <?php get_template_part('toggleposts')?>
         <div id="indic"></div>
