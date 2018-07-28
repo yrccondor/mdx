8b55c8a9c8768a0dac5e67a00b5ef30c0196be78
Yrccondor <yrc371106@hotmail.com>
2018-07-28 17:42:09 +0800

    Add author info card to 'post-like' page

--- page-postlike.php
+++ page-postlike.php
@@ -79,7 +79,7 @@ $pageType = 3;
                 <?php if($mdx_share_area=="all" || $mdx_share_area=="china"){include('includes/share_cn.php');}if($mdx_share_area=="all" || $mdx_share_area=="oversea"){include('includes/share_oversea.php');}?>
             </ul>
                 <i class="mdui-icon material-icons">&#xe54e;</i> <?php _e('没有标签','mdx');?><span class="mdui-text-color-black-disabled timeInPost" itemprop="datePublished"><?php if(mdx_get_option('mdx_post_edit_time')==="post"){?><i class="mdui-icon material-icons info-icon">&#xe192;</i> <?php the_time('Y-m-d');}else{?><i class="mdui-icon material-icons info-icon">&#xe3c9;</i> <?php the_modified_time('Y-m-d');}?></span>
-                <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?><?php comments_template();?>
+                <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?><?php if(mdx_get_option('mdx_author_card')=="true"){include_once("includes/author_card.php");}comments_template();?>
             </div>
 <?php get_template_part('toggleposts')?>
         <div id="indic"></div>
@@ -120,7 +120,7 @@ $pageType = 3;
                 <i class="mdui-icon material-icons">&#xe54e;</i> <?php _e('没有标签','mdx');?><span class="mdui-text-color-black-disabled timeInPost" itemprop="datePublished"><?php if(mdx_get_option('mdx_post_edit_time')==="post"){?><i class="mdui-icon material-icons info-icon">&#xe192;</i> <?php the_time('Y-m-d');}else{?><i class="mdui-icon material-icons info-icon">&#xe3c9;</i> <?php the_modified_time('Y-m-d');}?></span>
                 <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?>
             </div>
-<?php comments_template();?>
+            <?php if(mdx_get_option('mdx_author_card')=="true"){include_once("includes/author_card.php");}comments_template();?>
 <?php get_template_part('toggleposts')?>
         <div id="indic"></div>
         
@@ -157,7 +157,7 @@ $pageType = 3;
                 <?php if($mdx_share_area=="all" || $mdx_share_area=="china"){include('includes/share_cn.php');}if($mdx_share_area=="all" || $mdx_share_area=="oversea"){include('includes/share_oversea.php');}?>
             </ul>
                 <i class="mdui-icon material-icons">&#xe54e;</i> <?php _e('没有标签','mdx');?><span class="mdui-text-color-black-disabled timeInPost" itemprop="datePublished"><?php if(mdx_get_option('mdx_post_edit_time')==="post"){?><i class="mdui-icon material-icons info-icon">&#xe192;</i> <?php the_time('Y-m-d');}else{?><i class="mdui-icon material-icons info-icon">&#xe3c9;</i> <?php the_modified_time('Y-m-d');}?></span>
-                <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?><?php comments_template();?>
+                <div class="mdui-divider"></div><?php mdx_breadcrumbs();?></div></div><?php endwhile;?><?php if(mdx_get_option('mdx_author_card')=="true"){include_once("includes/author_card.php");}comments_template();?>
             </div>
 <div id="indic"></div>
 <?php }?>
