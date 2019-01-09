ee8b9885699c088024e5245d34003ef2536f05ab
AxtonYao <yrc371106@hotmail.com>
2019-01-10 00:42:01 +0800

    Improve performance

--- ajax-comment/main.php
+++ ajax-comment/main.php
@@ -8,13 +8,14 @@ if ( version_compare( $GLOBALS['wp_version'], '4.4-alpha', '<' ) ) {
 if(!function_exists('fa_ajax_comment_scripts')) :
 
     function fa_ajax_comment_scripts(){
-        // wp_enqueue_style( 'ajax-comment', get_template_directory_uri() . '/ajax-comment/app.css', array(), AC_VERSION );
-        wp_enqueue_script( 'ajax-comment', get_template_directory_uri() . '/ajax-comment/app.js', array(), AC_VERSION , true );
-        wp_localize_script( 'ajax-comment', 'ajaxcomment', array(
-            'ajax_url'   => admin_url('admin-ajax.php'),
-            'order' => get_option('comment_order'),
-            'formpostion' => 'top',
-        ) );
+        if(is_single() || is_page()){
+            wp_enqueue_script('ajax-comment', get_template_directory_uri().'/ajax-comment/app.js', array(), AC_VERSION , true);
+            wp_localize_script('ajax-comment', 'ajaxcomment', array(
+                'ajax_url'   => admin_url('admin-ajax.php'),
+                'order' => get_option('comment_order'),
+                'formpostion' => 'top',
+            ));
+        }
     }
 
 endif;
