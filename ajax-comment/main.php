4c07ae594b39e6b13d493810cf1b9b4d4bef5a1f
Yrccondor <yrc371106@hotmail.com>
2020-01-18 23:50:06 +0800

    A better js for commenting on mobile devices

--- ajax-comment/main.php
+++ ajax-comment/main.php
@@ -9,7 +9,8 @@ if(!function_exists('fa_ajax_comment_scripts')) :
 
     function fa_ajax_comment_scripts(){
         if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-            wp_enqueue_script( 'comment-reply' );
+            wp_enqueue_script( 'better-comment', get_template_directory_uri() . '/js/better_comment.js', array(), AC_VERSION , true );
+            // wp_enqueue_script( 'comment-reply' );
         }
         wp_enqueue_script( 'ajax-comment', get_template_directory_uri() . '/ajax-comment/app.js', array( 'jquery' ), AC_VERSION , true );
         wp_localize_script( 'ajax-comment', 'ajaxcomment', array(
