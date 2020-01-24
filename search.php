f319f2ba92009e5e2cabf35f2e8925cf7558c006
Yrccondor <yrc371106@hotmail.com>
2020-01-24 19:13:03 +0800

    Improve empty search page

--- search.php
+++ search.php
@@ -80,9 +80,9 @@
       <div class="theFirstPageSmall mdui-valign mdui-typo mdui-text-color-white-text mdui-color-theme"><h1 class="mdui-center mdui-text-center"><?php the_search_query();?><br><small><?php _e('搜索结果','mdx');?></small></h1></div>
     <div class="main-in-other">
       <main class="postList mdui-center" id="postlist">
-      <br><br><br><i class="mdui-icon material-icons mdui-center mdx-search-empty">&#xe565;</i><br><br><br>
-      <h1 class="mdui-center mdx-search-empty-text"><?php _e('前方似乎空空如也','mdx');?></h1>
-      <h2 class="mdui-center mdx-search-empty-text"><?php _e('什么也没找到，换个词搜搜试试？','mdx');?></h2><br><br>
+      <i class="mdui-icon material-icons mdui-center mdx-search-empty">&#xe880;</i>
+      <h1 class="mdui-center mdx-search-empty-text mdx-search-empty-title"><?php _e('这里似乎空空如也','mdx');?></h1>
+      <h2 class="mdui-center mdx-search-empty-text mdx-search-empty-end"><?php _e('什么也没找到，换个词搜索试试？','mdx');?></h2>
       </main>
     <?php endif;?>
 <?php get_footer();?>
