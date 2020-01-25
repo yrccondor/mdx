32976ae6d7c5649356bb82b91f2ca9492524c3dd
Hasan CAN <hasancan.net.tr@gmail.com>
2020-01-25 14:08:23 +0300

    xcv

--- gutenberg-src/README.md
+++
@@ -1,9 +0,0 @@
-# MDx Blocks
-
-> Gutenberg blocks for MDx theme
-
-MDx主题内置的Gutenberg区块源码
-
-运行`npm run start`会监听src文件夹的任何修改操作并实时生成开发环境的代码
-
-运行`npm run build`会生成用于生产环境的代码
--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form action="/" method="GET" role="search">
-    <div class="mdui-textfield">
-        <input class="mdui-textfield-input" type="text" name="s" id="mdx-search-widget" value="<?php the_search_query();?>" placeholder="<?php _e('搜索什么...','mdx');?>">
-    </div>
-    <input type="submit" class="mdui-btn mdui-btn-block mdui-color-theme-accent mdui-ripple" value="<?php _e('搜索','mdx');?>">
-</form>
