c8c1bc88b8348eb2dd8ba26501789e36ba96a36f
Yrccondor <yrc371106@hotmail.com>
2020-01-26 17:03:14 +0800

    Fix ajax comment

--- ajax-comment/main.php
+++ ajax-comment/main.php
@@ -10,9 +10,8 @@ if(!function_exists('fa_ajax_comment_scripts')) :
     function fa_ajax_comment_scripts(){
         if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
             wp_enqueue_script( 'better-comment', get_template_directory_uri() . '/js/better_comment.js', array(), AC_VERSION , true );
-            // wp_enqueue_script( 'comment-reply' );
         }
-        wp_enqueue_script( 'ajax-comment', get_template_directory_uri() . '/ajax-comment/app.js', array( 'jquery' ), AC_VERSION , true );
+        wp_enqueue_script( 'ajax-comment', get_template_directory_uri() . '/ajax-comment/app.js', array(), AC_VERSION , true );
         wp_localize_script( 'ajax-comment', 'ajaxcomment', array(
             'ajax_url'   => admin_url('admin-ajax.php'),
             'order' => get_option('comment_order'),
