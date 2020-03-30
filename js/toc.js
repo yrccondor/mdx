49d4cdb233153b87d14f8615d1c0d8ed195e9de3
yrccondor <yrc371106@hotmail.com>
2020-03-31 01:08:56 +0800

    Add TOC preview (Part 1)

--- js/toc.js
+++ js/toc.js
@@ -2,9 +2,12 @@ let tocShown = false;
 let titleArr = [];
 let firstClick = false;
 let isToc = true;
+let previewShown = false;
 
 $(function() {
-    addToc(getTitleListHtml());
+    let tocHTML = getTitleListHtml();
+    addToc(tocHTML[0]);
+    $(".PostMain").append('<div id="mdx-toc-preview">'+tocHTML[1]+'</div>');
     if(isToc){
         scrollToc(true);
     }
@@ -17,6 +20,7 @@ function getTitleListHtml() {
         return false;
     }
     let finalHtml = '<div class="mdui-list" id="mdx-toc">';
+    let finalPreview = '';
     let counter = 0;
     let title1 = 0;
     let title2 = 0;
@@ -29,6 +33,7 @@ function getTitleListHtml() {
             title2 = 0;
             title3 = 0;
             finalHtml += '<a class="mdui-list-item mdui-ripple mdx-toc-item" id="mdx-toc-'+counter+'-item" title="'+$(title).text()+'"><span>'+title1+'</span><div>'+$(title).text()+'</div></a>';
+            finalPreview += '<div class="mdx-toc-preview-h1 mdx-toc-preview-item mdui-color-theme" id="mdx-toc-'+counter+'-preview"></div>';
         }else if($(title)[0].tagName ==="H2"){
             title2++;
             title3 = 0;
@@ -36,6 +41,7 @@ function getTitleListHtml() {
                 title1 = 1;
             }
             finalHtml += '<a class="mdui-list-item mdui-ripple mdx-toc-item mdx-toc-item-h2" id="mdx-toc-'+counter+'-item" title="'+$(title).text()+'"><span>'+title1+'.'+title2+'</span><div>'+$(title).text()+'</div></a>';
+            finalPreview += '<div class="mdx-toc-preview-h2 mdx-toc-preview-item mdui-color-theme" id="mdx-toc-'+counter+'-preview"></div>';
         }else if($(title)[0].tagName ==="H3"){
             title3++;
             if(title1 === 0){
@@ -45,10 +51,11 @@ function getTitleListHtml() {
                 title2 = 1;
             }
             finalHtml += '<a class="mdui-list-item mdui-ripple mdx-toc-item mdx-toc-item-h3" id="mdx-toc-'+counter+'-item" title="'+$(title).text()+'"><span>'+title1+'.'+title2+'.'+title3+'</span><div>'+$(title).text()+'</div></a>';
+            finalPreview += '<div class="mdx-toc-preview-h3 mdx-toc-preview-item mdui-color-theme" id="mdx-toc-'+counter+'-preview"></div>';
         }
         counter++;
     }
-    return finalHtml+'</div>';
+    return [finalHtml+'</div>', finalPreview];
 }
 
 function addToc(titleList) {
@@ -120,9 +127,14 @@ function scrollToc(firstCall){
     if(tocShown || firstCall){
         let howFar = document.documentElement.scrollTop || document.body.scrollTop;
         $(".mdx-toc-item").removeClass("mdx-toc-read").removeClass("mdui-list-item-active");
+        $("#mdx-toc-preview > *").removeClass("mdx-toc-preview-item-active");
         let counter = 0;
         if(howFar >= $("article").offset().top + $("article").height() - 80){
-            $(".mdx-toc-item").addClass("mdx-toc-read")
+            $(".mdx-toc-item").addClass("mdx-toc-read");
+            if(previewShown){
+                $("#mdx-toc-preview").css("opacity", "0");
+                previewShown = false;
+            }
         }else{
             for(let i = 1; i < titleArr.length; i++){
                 if(howFar >= $("article *[data-mdxtoc='"+titleArr[i]+"']").offset().top - 80){
@@ -133,10 +145,21 @@ function scrollToc(firstCall){
                 }
             }
             if(howFar > $("article").offset().top - 140){
-                let item = $("#"+titleArr[counter]+"-item")
+                if(!previewShown){
+                    $("#mdx-toc-preview").css("opacity", "1");
+                    previewShown = true;
+                }
+                let item = $("#"+titleArr[counter]+"-item");
                 item.addClass("mdui-list-item-active");
+                $("#"+titleArr[counter]+"-preview").addClass("mdx-toc-preview-item-active");
+                $("#mdx-toc-preview").css("transform", "translateY(-"+((counter+1)*24-8)+"px)")
                 if(item[0].getBoundingClientRect().top + 48 > window.innerHeight){
                     $("#left-drawer").animate({scrollTop:document.getElementById("left-drawer").scrollTop + (item[0].getBoundingClientRect().top + 48 - window.innerHeight) + 8}, 150);
+                }
+            }else{
+                if(previewShown){
+                    $("#mdx-toc-preview").css("opacity", "0");
+                    previewShown = false;
                 }
             }
         }
