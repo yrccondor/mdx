73210cb88f79efd3abb5b4b9c6acb05555658107
yrccondor <yrc371106@hotmail.com>
2020-06-09 23:58:07 +0800

    Fix #108

--- js/login.js
+++ js/login.js
@@ -10,16 +10,16 @@ $(function(){
     $($("#registerform").children("p")[1]).addClass("mdui-textfield");
     $($("#registerform").children("p")[1]).children("input#user_email").addClass("mdui-textfield-input");
     $($("#registerform").children("p")[1]).children("label").addClass("mdui-textfield-label");
-    $("#user_pass").addClass("mdui-textfield-input");
-    $("#loginform").children(".user-pass-wrap").append($("#user_pass"));
-    $("#loginform").children(".user-pass-wrap").addClass("mdui-textfield");
-    $("#loginform").children(".user-pass-wrap").children("label").addClass("mdui-textfield-label");
-    $("#rememberme").after('<i class="mdui-checkbox-icon"></i>');
+    $("#user_pass, #authcode").addClass("mdui-textfield-input");
+    $("form[name='loginform']").children(".user-pass-wrap").append($("#user_pass"));
+    $("form[name='loginform']").children(".user-pass-wrap").addClass("mdui-textfield");
+    $("form[name='loginform']").children(".user-pass-wrap").children("label").addClass("mdui-textfield-label");
+    $("form[name='loginform'] #rememberme").after('<i class="mdui-checkbox-icon"></i>');
     $($(".mdui-checkbox-icon")[0]).after('<span>'+$(".forgetmenot").children("label").text()+'</span>');
     $(".forgetmenot").children("label").remove();
     $(".forgetmenot").after('<label class="mdui-checkbox">'+$(".forgetmenot").html()+"</label>");
     $(".forgetmenot").remove();
     $(".mdui-checkbox").addClass("forgetmenot");
-    $("#wp-submit").addClass("mdui-btn mdui-btn-raised");
+    $("#wp-submit, #submit").addClass("mdui-btn mdui-btn-raised");
     mdui.mutation();
 })
