d8319d6a38f334c33fdfb71671440eb6608391eb
yrccondor <yrc371106@hotmail.com>
2020-07-04 22:52:59 +0800

    Improve css svg inline

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
     plugins: [
         require('autoprefixer'),
         require('cssnano'),
+        require("postcss-inline-svg"),
         require("postcss-import")
     ]
 })
