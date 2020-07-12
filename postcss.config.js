78a93bb26a9241cac281bf455bd9410e8d79d1f1
yrccondor <yrc371106@hotmail.com>
2020-07-12 22:56:04 +0800

    Use clean-css instead of cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
     plugins: [
         require('autoprefixer'),
-        require('cssnano'),
+        require('postcss-clean')({
+            level: 2
+        }),
         require("postcss-inline-svg"),
         require("postcss-import")
     ]
