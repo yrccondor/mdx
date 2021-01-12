b049d42ca72c83714e66f78439e63bb9357d9d17
yrccondor <yrc371106@hotmail.com>
2021-01-13 03:54:27 +0800

    chore: update translation

--- gutenberg-src/src/mdx/mdx.js
+++ gutenberg-src/src/mdx/mdx.js
@@ -9,7 +9,7 @@ registerBlockType("mdx/fold", {
     title: __("MDx 折叠内容", "mdx"),
     icon: "editor-contract",
     category: "common",
-    keywords: [__("fold"), __("mdx"), __("折叠内容")],
+    keywords: ["mdx", __("折叠内容", "mdx")],
     attributes: {
         title: {
             type: "string",
@@ -87,7 +87,7 @@ registerBlockType("mdx/warning", {
     title: __("MDx 警告内容", "mdx"),
     icon: "warning",
     category: "common",
-    keywords: [__("warning"), __("mdx"), __("警告内容")],
+    keywords: ["mdx", __("警告内容", "mdx")],
     attributes: {
         title: {
             type: "string",
@@ -144,7 +144,7 @@ registerBlockType("mdx/progress", {
     title: __("MDx 进度指示器", "mdx"),
     icon: "minus",
     category: "common",
-    keywords: [__("progress"), __("mdx"), __("进度指示器")],
+    keywords: ["mdx", __("进度指示器", "mdx")],
     attributes: {
         progress: {
             type: "string"
@@ -181,7 +181,7 @@ registerBlockType("mdx/github", {
         <defs><style></style></defs><path d="M950.93 512q0 143.43-83.75 257.97T650.9 928.55q-15.43 2.85-22.6-4.02t-7.17-17.12V786.87q0-55.44-29.7-81.11 32.55-3.44 58.6-10.32t53.68-22.3T750 635.1t30.28-59.98 11.7-86.01q0-69.12-45.13-117.7 21.14-52-4.53-116.58-16.02-5.12-46.3 6.29t-52.6 25.16l-21.72 13.68Q568.54 285.1 512 285.1t-109.71 14.85q-9.15-6.3-24.29-15.43t-47.69-22.02-49.15-7.68q-25.16 64.58-4.02 116.59Q232 419.99 232 489.1q0 48.56 11.7 85.72t30 59.98 46 38.25 53.68 22.3 58.6 10.32q-22.83 20.56-28.02 58.88-12 5.7-25.75 8.56t-32.55 2.85-37.45-12.29T276.48 728q-10.83-18.28-27.72-29.7t-28.3-13.67l-11.42-1.69q-12 0-16.6 2.56t-2.85 6.59 5.12 7.97 7.46 6.88l4.02 2.85q12.58 5.7 24.87 21.72t18 29.11l5.7 13.17q7.46 21.72 25.16 35.1T318.17 826t39.72 4.03 31.74-1.98l13.17-2.27q0 21.73.29 50.84t.3 30.86q0 10.32-7.47 17.12t-22.82 4.02Q240.57 884.6 156.82 770.05T73.07 512.07q0-119.44 58.88-220.3t159.74-159.75T512 73.14t220.3 58.88 159.75 159.75 58.88 220.3z" fill="#000"></path>
     </svg>,
     category: "common",
-    keywords: [__("github"), __("mdx"), __("github")],
+    keywords: [__("github"), "mdx"],
     attributes: {
         author: {
             type: "string"
@@ -303,7 +303,7 @@ registerBlockType("mdx/post", {
     title: __("MDx 文章信息卡", "mdx"),
     icon: "format-aside",
     category: "common",
-    keywords: [__("post"), __("mdx"), __("文章信息卡")],
+    keywords: ["mdx", __("文章信息卡", "mdx")],
     attributes: {
         url: {
             type: "string"
@@ -362,7 +362,7 @@ registerBlockType("mdx/ad", {
     title: __("MDx 广告", "mdx"),
     icon: "media-document",
     category: "common",
-    keywords: [__("ad", "mdx", "广告")],
+    keywords: ["mdx", __("广告", "mdx")],
     edit: () => {
         return (
             <div style={{
