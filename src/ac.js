734a98daf79c0649df202248b4bf5db333af1d7d
yrccondor <yrc371106@hotmail.com>
2021-01-13 20:08:49 +0800

    fix: do not layout multi-col post list if there is no post

--- src/ac.js
+++ src/ac.js
@@ -183,22 +183,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
     if (document.body.classList.contains('mdx-wide-post-list')) {
         const postList = document.getElementById('postlist');
-        let gutter = 30;
-        if (postList.getElementsByClassName('indexgaid').length > 0) {
-            gutter = 20;
-        }
-        window.mdxMasonry = new Masonry(postList, {
-            itemSelector: '.post-item',
-            columnWidth: '.post-item',
-            gutter,
-            stagger: 10,
-            percentPosition: true
-        });
-        document.addEventListener('lazyloaded', (e) => {
-            if (e.target.matches('#postlist > .post-item img')) {
-                window.mdxMasonry.layout()
+        if (postList.getElementsByClassName('post-item').length > 0) {
+            let gutter = 30;
+            if (postList.getElementsByClassName('indexgaid').length > 0) {
+                gutter = 20;
             }
-        })
+            window.mdxMasonry = new Masonry(postList, {
+                itemSelector: '.post-item',
+                columnWidth: '.post-item',
+                gutter,
+                stagger: 10,
+                percentPosition: true
+            });
+            document.addEventListener('lazyloaded', (e) => {
+                if (e.target.matches('#postlist > .post-item img')) {
+                    window.mdxMasonry.layout()
+                }
+            })
+        }
     }
 })
 
