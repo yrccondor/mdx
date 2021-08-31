297981309015aa2c834b65a1193f9169501aefab
AH-dark <ahdark@outlook.com>
2021-08-31 19:54:21 +0800

    Optimization

--- header.php
+++ header.php
@@ -1,46 +1,63 @@
-﻿<!DOCTYPE html>
+﻿<?php
+//引用全局变量
+global $wp;
+global $files_root;
+global $page, $paged;
+
+// 获取当前页面Title
+$title = "";
+$title .= wp_title('-', true, 'right');
+$title .= get_bloginfo('name');
+$site_description = get_bloginfo('description', 'display');
+if ($site_description && (is_home() || is_front_page())) $title .= " - $site_description";
+if ($paged >= 2 || $page >= 2) $title .= ' - ' . sprintf(__('第 %s 页'), max($paged, $page));
+
+// 获取当前页面分享图
+$opt_mdx_index_img = mdx_get_option('mdx_index_img');
+$coverPicIsBing = (bool)substr($opt_mdx_index_img, 0, 6) == "--Bing";
+$index_image = !(!is_single() && !is_page() && $coverPicIsBing)?((is_single() || is_page())?(wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full')[0]??""):$opt_mdx_index_img):mdx_get_option('mdx_side_head');
+
+// 获取当前页面社会分享描述
+$social_describe = "";
+$mdx_des = mdx_get_option('mdx_seo_des');
+if (is_single() || is_page()) {
+    if (post_password_required()) {
+        $social_describe = translate('这篇文章受密码保护，输入密码后才能查看。', 'mdx');
+    } else {
+        $social_describe = mdx_get_post_excerpt($post, 100);
+    }
+} else if ($mdx_des != '') {
+    $social_describe = $mdx_des;
+} else {
+    $social_describe = get_bloginfo('description', 'display');
+}
+
+// PS: 判断页面类型的我还能再优化，但实在太懒（划掉）
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=<?php if (mdx_get_option('mdx_allow_scale') == 'false') {
-        echo '1, user-scalable=no';
-    } else {
-        echo '5';
-    } ?>">
-    <?php if (mdx_get_option('mdx_speed_pre') == 'true' && !is_404()) { ?>
-        <?php if (is_home()) {
-            $mdx_js_name2 = 'js';
-        } else if (is_category() || is_archive() || is_search()) {
-            $mdx_js_name2 = 'ac';
-        } else if (is_single()) {
-            $mdx_js_name2 = 'post';
-        } else if (is_page()) {
-            $mdx_js_name2 = 'page';
-        } else {
-            $mdx_js_name2 = 'js';
-        }
-        global $files_root;
-        ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=<?php echo (mdx_get_option('mdx_allow_scale') == 'false')?"1, user-scalable=no":"5"; ?>">
+    <?php if (mdx_get_option('mdx_speed_pre') == 'true' && !is_404()) {
+        if (is_home()) { $mdx_js_name2 = 'js'; } else if (is_category() || is_archive() || is_search()) { $mdx_js_name2 = 'ac'; } else if (is_single()) { $mdx_js_name2 = 'post'; } else if (is_page()) { $mdx_js_name2 = 'page'; } else { $mdx_js_name2 = 'js'; } ?>
+
         <link rel="preload" href="<?php echo $files_root; ?>/js/common.js?ver=<?php echo get_option("mdx_version_commit"); ?>" as="script">
         <link rel="preload" href="<?php echo $files_root; ?>/js/<?php echo $mdx_js_name2 ?>.js?ver=<?php echo get_option("mdx_version_commit"); ?>" as="script">
-        <link rel="preload" href="<?php echo $files_root; ?>/mdui/icons/material-icons/<?php if (mdx_get_option("mdx_md2") == "false") { ?>MaterialIcons-Regular.woff2<?php } else { ?>material_2_icon_font.woff2<?php } ?>" as="font" type="font/woff2" crossorigin>
+        <link rel="preload" href="<?php echo $files_root; ?>/mdui/icons/material-icons/<?php echo (mdx_get_option("mdx_md2") == "false")?"MaterialIcons-Regular.woff2":"material_2_icon_font.woff2" ?>" as="font" type="font/woff2" crossorigin>
         <?php if (mdx_get_option('mdx_md2') == "true" && mdx_get_option('mdx_md2_font') == "true") { ?>
             <link rel="preload" href="<?php echo $files_root; ?>/fonts/Montserrat-Regular.woff2" as="font" type="font/woff2" crossorigin>
             <link rel="preload" href="<?php echo $files_root; ?>/fonts/Montserrat-SemiBold.woff2" as="font" type="font/woff2" crossorigin>
-        <?php }
-    } ?>
+        <?php } ?>
+    <?php } ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Theme developed by AxtonYao -->
+    <meta name="author" content="AxtonYao" />
     <?php if (mdx_get_option('mdx_safari') == "true") { ?>
         <link rel="mask-icon" href="<?php echo mdx_get_option('mdx_svg'); ?>" color="<?php echo mdx_get_option('mdx_svg_color'); ?>">
     <?php } ?>
     <?php if (mdx_get_option("mdx_title_med") == "diy") { ?>
-        <title itemprop="name"><?php global $page, $paged;
-            wp_title('-', true, 'right');
-            bloginfo('name');
-            $site_description = get_bloginfo('description', 'display');
-            if ($site_description && (is_home() || is_front_page())) echo " - $site_description";
-            if ($paged >= 2 || $page >= 2) echo ' - ' . sprintf(__('第 %s 页'), max($paged, $page)); ?>
-        </title>
+        <title itemprop="name"><?php echo $title; ?></title>
     <?php } ?>
     <?php if (is_single() || is_page()) {
         if (function_exists('get_query_var')) {
@@ -52,85 +69,25 @@
         }
     } ?>
     <?php if (!is_404()) { ?>
-        <meta property="og:title" content="<?php global $page, $paged;
-        wp_title('-', true, 'right');
-        bloginfo('name');
-        $site_description = get_bloginfo('description', 'display');
-        if ($site_description && (is_home() || is_front_page())) echo " - $site_description";
-        if ($paged >= 2 || $page >= 2) echo ' - ' . sprintf(__('第 %s 页'), max($paged, $page)); ?>">
+        <meta property="og:title" content="<?php echo $title; ?>">
         <meta property="og:type" content="article">
-        <meta property="og:url" content="<?php global $wp;
+        <meta property="og:url" content="<?php
         $mdx_current_url = mdx_get_now_url(is_single(), isset($post) ? (int)$post->ID : 0);
         echo $mdx_current_url; ?>">
         <?php
         $mdx_des = mdx_get_option('mdx_seo_des');
         $mdx_s_key = mdx_get_option('mdx_seo_key');
         $mdx_a_des = mdx_get_option('mdx_auto_des'); ?>
-        <meta property="og:description" content="<?php if (is_single() || is_page()) {
-            if (post_password_required()) {
-                _e('这篇文章受密码保护，输入密码后才能查看。', 'mdx');
-            } else {
-                echo mdx_get_post_excerpt($post, 100);
-            }
-        } else if ($mdx_des != '') {
-            echo $mdx_des;
-        } else {
-            bloginfo('description', 'display');
-        } ?>">
-        <?php $mdx_index_img = mdx_get_option('mdx_index_img');
-        if (!((!(is_single() || is_page())) && substr($mdx_index_img, 0, 6) == "--Bing")) { ?>
-            <meta property="og:image" content="<?php if (is_single() || is_page()) {
-                $mdx_post_img = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
-                if (isset($mdx_post_img[0])) {
-                    echo $mdx_post_img[0];
-                } else {
-                    echo "";
-                }
-            } else {
-                echo $mdx_index_img;
-            } ?>">
-        <?php } ?>
+        <meta property="og:description" content="<?php echo $social_describe; ?>">
+        <meta property="og:image" content="<?php echo $index_image; ?>">
         <meta name="twitter:card" content="summary">
-        <meta name="twitter:title" content="<?php wp_title('-', true, 'right');
-        bloginfo('name');
-        if ($site_description && (is_home() || is_front_page())) echo " - $site_description";
-        if ($paged >= 2 || $page >= 2) echo ' - ' . sprintf(__('第 %s 页'), max($paged, $page)); ?>">
-        <meta name="twitter:description" content="<?php if (is_single() || is_page()) {
-            if (post_password_required()) {
-                _e('这篇文章受密码保护，输入密码后才能查看。', 'mdx');
-            } else {
-                echo mdx_get_post_excerpt($post, 100);
-            }
-        } else if ($mdx_des != '') {
-            echo $mdx_des;
-        } else {
-            bloginfo('description', 'display');
-        } ?>">
+        <meta name="twitter:title" content="<?php echo $title; ?>">
+        <meta name="twitter:description" content="<?php echo $social_describe; ?>">
         <meta name="twitter:url" content="<?php echo $mdx_current_url; ?>">
-        <?php if (!((!(is_single() || is_page())) && substr($mdx_index_img, 0, 6) == "--Bing")) { ?>
-            <meta name="twitter:image" content="<?php if (is_single() || is_page()) {
-                if ($mdx_post_img !== false && $mdx_post_img[0] != "") {
-                    echo $mdx_post_img[0];
-                } else {
-                    echo "";
-                }
-            } else {
-                echo $mdx_index_img;
-            } ?>">
-            <meta itemprop="name" content="<?php wp_title('-', true, 'right');
-            bloginfo('name');
-            if ($site_description && (is_home() || is_front_page())) echo " - $site_description";
-            if ($paged >= 2 || $page >= 2) echo ' - ' . sprintf(__('第 %s 页'), max($paged, $page)); ?>">
-            <meta itemprop="image" content="<?php if (is_single() || is_page()) {
-                if ($mdx_post_img !== false && $mdx_post_img[0] != "") {
-                    echo $mdx_post_img[0];
-                } else {
-                    echo "";
-                }
-            } else {
-                echo $mdx_index_img;
-            } ?>">
-        <?php }
+        <meta name="twitter:image" content="<?php echo $index_image; ?>">
+        <meta itemprop="name" content="<?php echo $title; ?>">
+        <meta itemprop="image" content="<?php echo $index_image; ?>">
+        <?php
         if ($mdx_des !== '') {
             if ($mdx_a_des === 'true') {
                 if (is_single() || is_page()) {
@@ -173,5 +130,6 @@
         <meta name="mdx-main-color" content="<?php echo $mdx_theme_color; ?>">
     <?php } ?>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-    <?php wp_head(); ?><?php echo htmlspecialchars_decode(mdx_get_option('mdx_head_js')); ?>
+    <?php wp_head(); ?>
+    <?php echo htmlspecialchars_decode(mdx_get_option('mdx_head_js')); ?>
 </head>
